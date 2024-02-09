8ebd2668fc0597b4026bf9d603a7dc014ef4e268
Rohail Khan <Rohailk@outlook.ie>
2024-02-09 00:50:02 +0000

    Added previous work on the extension

---
+++ Extension/main.js
@@ -0,0 +1,19 @@
+
+function insertCSSWhenElementAppears() {
+    let checkExist = setInterval(function() {
+        let element = document.querySelector('.modal.fade');
+        if (element) {
+            clearInterval(checkExist);
+            let link = document.createElement('link');
+            link.href = chrome.runtime.getURL('main.css');
+            link.rel = 'stylesheet';
+            document.head.appendChild(link);
+            console.log('CSS injected!');
+        }
+        else {
+            console.log('Waiting for webpage to load...');
+        }
+    }, 1000); // checks every 1000ms
+}
+
+insertCSSWhenElementAppears();
